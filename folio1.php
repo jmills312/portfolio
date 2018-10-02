76a7fdd22f5b2d27343f3cbe352828262c7fd36c
jared <jared@jaredmc.com>
2018-10-02 17:58:19 -0500

    new assets and layout

--- folio1.php
+++ folio1.php
@@ -2,27 +2,35 @@
 
 <!-- nav -->
 <?php include 'nav.php'?>
-
-<section id="onboarding">
+<!-- top -->
+<section class="folio1" id="onboarding">
 	<div class="container">
 		<div class="row">
-
 			<div class="col-xs-12">
 				<h2 class="text-center">onboarding dashboard</h2>
 			</div>
 		</div><!--/row-->
-		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2 dashboard">
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="dash-header">
-								<img class="cnvr_logo" src="assets/img/portfolio/cnvr-logo.png" width="19px" height="17px" alt="conversant logo">
+		<div class="onboard-svg">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 dashboard">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="dash-header">
+									<img class="cnvr_logo" src="assets/img/portfolio/cnvr-logo.png" width="19px" height="17px" alt="conversant logo">
+								</div>
+						</div>
+					</div><!-- row -->
+					<!--left nav -->
+					<div class="col-md-3 col-sm-4 nav_items">
+						<div class="left_nav">
+							<div class="nav_item">
+								<div class="col-md-4 col-sm-4">
+									<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
+								</div>
+								<div class="col-sm-8">
+									<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
+								</div>
 							</div>
-					</div>
-				</div><!-- row -->
-				<!--left nav -->
-				<div class="col-md-3 col-sm-4 nav_items">
-					<div class="left_nav">
 						<div class="nav_item">
 							<div class="col-md-4 col-sm-4">
 								<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
@@ -31,86 +39,79 @@
 								<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
 							</div>
 						</div>
-					<div class="nav_item">
-						<div class="col-md-4 col-sm-4">
-							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
+						<div class="nav_item">
+							<div class="col-md-4 col-sm-4">
+								<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
+							</div>
+							<div class="col-sm-8">
+								<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
+							</div>
 						</div>
-						<div class="col-sm-8">
-							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
+						<div class="nav_item">
+							<div class="col-md-4 col-sm-4">
+								<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
+							</div>
+							<div class="col-sm-8">
+								<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
+							</div>
 						</div>
-					</div>
-					<div class="nav_item">
-						<div class="col-md-4 col-sm-4">
-							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
+						<div class="nav_item">
+							<div class="col-md-4 col-sm-4">
+								<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
+							</div>
+							<div class="col-sm-8">
+								<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
+							</div>
 						</div>
-						<div class="col-sm-8">
-							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
+						<div class="nav_item">
+							<div class="col-md-4 col-sm-4">
+								<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
+							</div>
+							<div class="col-sm-8">
+								<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
+							</div>
+							</div>
+						</div><!--/left nav-->
+					</div><!--/ col 3-->
+					<div class="col-md-9 col-sm-8 main">
+						<div class="col-sm-3">
+							<div class="high_task task_one"></div>
+						</div><!--/ col-sm-2-->
+						<div class="col-sm-3">
+							<div class="high_task task_two"></div>
+						</div><!--/ col-sm-2-->
+						<div class="col-sm-3">
+							<div class="high_task task_three"></div>
+						</div><!--/ col-sm-2-->
+						<div class="col-sm-3">
+							<div class="high_task task_four"></div>
+						</div><!--/ col-sm-2-->
+						<div class="row">
+							<div class="col-sm-2">				
+								<svg class="checkmark check1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+							</div><!--/col-sm-4-->
+							<div class="col-sm-8 task_line" data-aos="fade-right" data-aos-duration="1000"></div>
 						</div>
-					</div>
-					<div class="nav_item">
-						<div class="col-md-4 col-sm-4">
-							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
+						<div class="row">
+							<div class="col-sm-2">				
+								<svg class="checkmark check2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+							</div><!--/col-sm-4-->
+							<div class="col-sm-8 task_line" data-aos="fade-right" data-aos-duration="1200"></div>
 						</div>
-						<div class="col-sm-8">
-							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
+						<div class="row">
+							<div class="col-sm-2">				
+								<svg class="checkmark check3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+							</div><!--/col-sm-4-->
+							<div class="col-sm-8 task_line" data-aos="fade-right" data-aos-duration="1400"></div>
 						</div>
-					</div>
-					<div class="nav_item">
-						<div class="col-md-4 col-sm-4">
-							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
-						</div>
-						<div class="col-sm-8">
-							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
-						</div>
-					</div>
-					<div class="nav_item">
-						<div class="col-md-4 col-sm-4">
-							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
-						</div>
-						<div class="col-sm-8">
-							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
-						</div>
-						</div>
-					</div><!--/left nav-->
-				</div><!--/ col 3-->
-				<div class="col-md-9 col-sm-8 main">
-					<div class="col-sm-3">
-						<div class="high_task task_one"></div>
-					</div><!--/ col-sm-2-->
-					<div class="col-sm-3">
-						<div class="high_task task_two"></div>
-					</div><!--/ col-sm-2-->
-					<div class="col-sm-3">
-						<div class="high_task task_three"></div>
-					</div><!--/ col-sm-2-->
-					<div class="col-sm-3">
-						<div class="high_task task_four"></div>
-					</div><!--/ col-sm-2-->
-					<div class="row">
-						<div class="col-sm-2">				
-							<svg class="checkmark check1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
-						</div><!--/col-sm-4-->
-						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="800"></div>
-					</div>
-					<div class="row">
-						<div class="col-sm-2">				
-							<svg class="checkmark check2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
-						</div><!--/col-sm-4-->
-						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="1400"></div>
-					</div>
-					<div class="row">
-						<div class="col-sm-2">				
-							<svg class="checkmark check3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
-						</div><!--/col-sm-4-->
-						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="2000"></div>
-					</div>
-			</div><!--/ col 8-->
-		</div><!--/row-->
+				</div><!--/ col 8-->
+			</div><!--/row-->
 	</div><!--/container-->
 </section>
-<article>
+<!-- main content -->
+<article class="folio1">
 	<div class="container">
-		<div class="col-sm-6 col-sm-offset-3">
+		<div class="col-md-6 col-md-offset-3">
 			<h3>Role</h3>
 			<p>As the lead UX Architect and Designer I was involved with all stages of the project - concept through hi-fi mocks (delivered by UI team). This included attending the kickoff meeting to understand the project at a high level, initial requirement gathering stages, research, and design phase. 
 			</p>
@@ -118,25 +119,33 @@
 			</p>
 
 		<h3>Project Goals</h3>
-			<ul>
+			<ul class="m-bottom">
 				<li>To increase early client adoption and usage of Conversant Access</li>
 				<li>To educate the client with onboarding launch process to allow for earlier client resource allocation and shorter launch timing</li>
 				<li>To provide clients with consistent ongoing launch status and timing updates</li>
 				<li>To provide clients with standardized information sharing among their team.  This will decrease miscommunication during onboarding</li>
 				<li>To provide account management with single point of reference for communicating launch timing, thereby eliminating miscommunication with the client and internal teams </li>
 			</ul>
-
-		<h3>Problem</h3>
-		<p>Conversant has a dedicated Pre-Sales Account Management team responsible for onboarding new clients. Currently the team utilizes spreadsheets, email, and phone calls to complete necessary onboarding tasks.
-		</p>
-		
-	<div class="pain-points">
-		<h3>Pain Points</h3>
-			<div class="col-sm-5">			
-				<img src="assets/img/portfolio/onboard_user.svg" alt="onboarding user">
-			</div><!--/col sm 4-->
-			<div class="col-sm-7">
-				<ul>
+		<div class="problem m-bottom">
+			<h3>Problem</h3>
+			<p>Conversant has a dedicated Pre-Sales Account Management team responsible for onboarding new clients. Currently the team utilizes spreadsheets, email, and phone calls to complete necessary onboarding tasks.
+			</p>
+			<div class="row">
+				<div class="col-sm-6">
+					<img src="assets/img/portfolio/xls.svg" alt="xls doc">
+				</div>
+				<div class="col-sm-6">
+					<img src="assets/img/portfolio/phone.svg" alt="Phone">
+				</div>
+			</div><!--/row -->
+		</div><!--/problem-->
+		<div class="row pain-points m-bottom">	
+			<div class="col-md-5">
+				<h3>Pain Points</h3>			
+				<img class="m-bottom" src="assets/img/portfolio/onboard_user.svg" alt="onboarding user">
+			</div><!--/col sm 5-->
+			<div class="col-md-7">
+				<ul class="m-bottom2">
 					<li>Time consuming for AM’s to follow up with clients via email and phone to ensure tasks are completed </li>
 					<li>Tasks maintained in a spreadsheet are more prone to erroneous entries</li>
 					<li>Excel docs are not reader friendly</li>
@@ -144,52 +153,47 @@
 					<li>Current asset and documentation delivery is done via email causing bottlenecks</li>
 				</ul>
 			</div>
-		</div><!--/pain points-->
+		</div><!--/row-->
 	</div><!--/col sm 6-->
-	<div class="col-sm-12">
-		<div class="col-sm-8 col-sm-offset-2">
-			<p>Example of high level task (Online Integration) with associated sub-tasks:</p>
-			<img class="task-list" src="assets/img/portfolio/example_tasks.png" alt="sample task list">
-		</div><!-- col sm 8-->
-		<div class="col-sm-6 col-sm-offset-3">
-		<h3>Competitive Analysis</h3>
-		<p>The name “Dashboard” was misleading,  as our goal was to design a task management system — tailored to the AM teams needs. A dashboard typically provides data visualization for KPIs.
-		</p>
-		<p>Consequently, I focused my research on task oriented features found in project management systems, such as <a href="http:www.asana.com">Asana</a> and <a href="http:www.trello.com">Trello</a>.
-		</p>
-	</div>
-	</div><!--/col sm 12-->
+	<div class="col-md-12">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<p class="remove-m">Example of high level task (Online Integration) with associated sub-tasks:</p>
+			</div>
+		</div><!--/row-->
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<img class="task-list m-bottom2" src="assets/img/portfolio/example_tasks.png" alt="sample task list">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<h3>Competitive Analysis</h3>
+				<p>The name “Dashboard” was misleading,  as our goal was to design a task management system tailored to the AM teams needs. A dashboard typically provides data visualization for KPIs.
+				</p>
+				<p>Consequently, I focused my research on task oriented features found in project management systems, such as <a href="http://www.asana.com" target="blank">Asana</a> and <a href="http://www.trello.com" target="blank">Trello</a>.
+				</p>
+			</div><!-- col md 6-->
+		</div>
+
+		<div class="row">
+			<div class="col-md-6 col-md-offset-2">
+				<h3>AM Flowchart</h3>
+			</div>
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<img src="assets/img/portfolio/am-flowchart1.svg" alt="onboarding flowchart">
+				<img src="assets/img/portfolio/am-flowchart2.svg" alt="onboarding flowchart">
+				<img src="assets/img/portfolio/am-flowchart3.svg" alt="onboarding flowchart">
+			</div>
+		</div>
 
 
-		
+
+
 	</div><!-- /container-->
-
 </article>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
