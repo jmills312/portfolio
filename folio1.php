59c50818dbdbe9352267a14956ee9c6d80303f51
jared <jared@jaredmc.com>
2018-10-02 11:48:04 -0500

    add new images and content

--- folio1.php
+++ folio1.php
@@ -1,18 +1,16 @@
 <?php include 'doc_head.html' ?>
 
 <!-- nav -->
-<?php include 'nav.php' ?>
+<?php include 'nav.php'?>
 
 <section id="onboarding">
 	<div class="container">
-
-
 		<div class="row">
+
 			<div class="col-xs-12">
 				<h2 class="text-center">onboarding dashboard</h2>
 			</div>
 		</div><!--/row-->
-
 		<div class="row">
 			<div class="col-sm-8 col-sm-offset-2 dashboard">
 				<div class="row">
@@ -75,7 +73,6 @@
 						</div>
 					</div><!--/left nav-->
 				</div><!--/ col 3-->
-
 				<div class="col-md-9 col-sm-8 main">
 					<div class="col-sm-3">
 						<div class="high_task task_one"></div>
@@ -90,46 +87,112 @@
 						<div class="high_task task_four"></div>
 					</div><!--/ col-sm-2-->
 					<div class="row">
-						<div class="col-sm-2 check">				
-							<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+						<div class="col-sm-2">				
+							<svg class="checkmark check1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
 						</div><!--/col-sm-4-->
-						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="750"></div>
+						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="800"></div>
 					</div>
 					<div class="row">
-						<div class="col-sm-2 check">				
-							<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+						<div class="col-sm-2">				
+							<svg class="checkmark check2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
 						</div><!--/col-sm-4-->
-						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="750"></div>
+						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="1400"></div>
 					</div>
 					<div class="row">
-						<div class="col-sm-2 check">				
-							<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+						<div class="col-sm-2">				
+							<svg class="checkmark check3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
 						</div><!--/col-sm-4-->
-						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="750"></div>
+						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="2000"></div>
 					</div>
-			
-				
-
-
-				
-
-
-		
-
-		
-
-
-			
-
-
-
-	
-
-
 			</div><!--/ col 8-->
 		</div><!--/row-->
 	</div><!--/container-->
 </section>
+<article>
+	<div class="container">
+		<div class="col-sm-6 col-sm-offset-3">
+			<h3>Role</h3>
+			<p>As the lead UX Architect and Designer I was involved with all stages of the project - concept through hi-fi mocks (delivered by UI team). This included attending the kickoff meeting to understand the project at a high level, initial requirement gathering stages, research, and design phase. 
+			</p>
+			<p>I worked directly with key stakeholders through the requirements gathering stage and research phase. The lead AM for the presales team was my main point of contact. She  worked  with me to understand their workflow, challenges, and goals of the project. Technical questions and concerns were run through the product manager.
+			</p>
+
+		<h3>Project Goals</h3>
+			<ul>
+				<li>To increase early client adoption and usage of Conversant Access</li>
+				<li>To educate the client with onboarding launch process to allow for earlier client resource allocation and shorter launch timing</li>
+				<li>To provide clients with consistent ongoing launch status and timing updates</li>
+				<li>To provide clients with standardized information sharing among their team.  This will decrease miscommunication during onboarding</li>
+				<li>To provide account management with single point of reference for communicating launch timing, thereby eliminating miscommunication with the client and internal teams </li>
+			</ul>
+
+		<h3>Problem</h3>
+		<p>Conversant has a dedicated Pre-Sales Account Management team responsible for onboarding new clients. Currently the team utilizes spreadsheets, email, and phone calls to complete necessary onboarding tasks.
+		</p>
+		
+	<div class="pain-points">
+		<h3>Pain Points</h3>
+			<div class="col-sm-5">			
+				<img src="assets/img/portfolio/onboard_user.svg" alt="onboarding user">
+			</div><!--/col sm 4-->
+			<div class="col-sm-7">
+				<ul>
+					<li>Time consuming for AM’s to follow up with clients via email and phone to ensure tasks are completed </li>
+					<li>Tasks maintained in a spreadsheet are more prone to erroneous entries</li>
+					<li>Excel docs are not reader friendly</li>
+					<li>Reduced transparency with clients, since they don’t have access to task lists</li>
+					<li>Current asset and documentation delivery is done via email causing bottlenecks</li>
+				</ul>
+			</div>
+		</div><!--/pain points-->
+	</div><!--/col sm 6-->
+	<div class="col-sm-12">
+		<div class="col-sm-8 col-sm-offset-2">
+			<p>Example of high level task (Online Integration) with associated sub-tasks:</p>
+			<img class="task-list" src="assets/img/portfolio/example_tasks.png" alt="sample task list">
+		</div><!-- col sm 8-->
+		<div class="col-sm-6 col-sm-offset-3">
+		<h3>Competitive Analysis</h3>
+		<p>The name “Dashboard” was misleading,  as our goal was to design a task management system — tailored to the AM teams needs. A dashboard typically provides data visualization for KPIs.
+		</p>
+		<p>Consequently, I focused my research on task oriented features found in project management systems, such as <a href="http:www.asana.com">Asana</a> and <a href="http:www.trello.com">Trello</a>.
+		</p>
+	</div>
+	</div><!--/col sm 12-->
+
+
+		
+	</div><!-- /container-->
+
+</article>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!-- custom js -->
 <?php include 'custom.js'?>
