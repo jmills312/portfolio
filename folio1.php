33c3a198bdea3094973854c41f4189f133af4d9b
jared <jared@jaredmc.com>
2018-10-01 19:09:06 -0500

    new dash effects

--- folio1.php
+++ folio1.php
@@ -27,50 +27,50 @@
 					<div class="left_nav">
 						<div class="nav_item">
 							<div class="col-md-4 col-sm-4">
-								<div class="nav_box"></div>
+								<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
 							</div>
 							<div class="col-sm-8">
-								<div class="nav_line"></div>
+								<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
 							</div>
 						</div>
 					<div class="nav_item">
 						<div class="col-md-4 col-sm-4">
-							<div class="nav_box"></div>
+							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
 						</div>
 						<div class="col-sm-8">
-							<div class="nav_line"></div>
+							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
 						</div>
 					</div>
 					<div class="nav_item">
 						<div class="col-md-4 col-sm-4">
-							<div class="nav_box"></div>
+							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
 						</div>
 						<div class="col-sm-8">
-							<div class="nav_line"></div>
+							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
 						</div>
 					</div>
 					<div class="nav_item">
 						<div class="col-md-4 col-sm-4">
-							<div class="nav_box"></div>
+							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
 						</div>
 						<div class="col-sm-8">
-							<div class="nav_line"></div>
+							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
 						</div>
 					</div>
 					<div class="nav_item">
 						<div class="col-md-4 col-sm-4">
-							<div class="nav_box"></div>
+							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
 						</div>
 						<div class="col-sm-8">
-							<div class="nav_line"></div>
+							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
 						</div>
 					</div>
 					<div class="nav_item">
 						<div class="col-md-4 col-sm-4">
-							<div class="nav_box"></div>
+							<div class="nav_box" data-aos="zoom-in" data-aos-duration="1000"></div>
 						</div>
 						<div class="col-sm-8">
-							<div class="nav_line"></div>
+							<div class="nav_line" data-aos="zoom-in" data-aos-duration="1200"></div>
 						</div>
 						</div>
 					</div><!--/left nav-->
@@ -90,22 +90,22 @@
 						<div class="high_task task_four"></div>
 					</div><!--/ col-sm-2-->
 					<div class="row">
-						<div class="col-sm-3 check">				
+						<div class="col-sm-2 check">				
 							<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
 						</div><!--/col-sm-4-->
-						<div class="col-sm-8"></div>
+						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="750"></div>
 					</div>
 					<div class="row">
-						<div class="col-sm-3 check">				
+						<div class="col-sm-2 check">				
 							<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
 						</div><!--/col-sm-4-->
-						<div class="col-sm-8"></div>
+						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="750"></div>
 					</div>
 					<div class="row">
-						<div class="col-sm-3 check">				
+						<div class="col-sm-2 check">				
 							<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
 						</div><!--/col-sm-4-->
-						<div class="col-sm-8"></div>
+						<div class="col-sm-8"><img class="task_line" src="assets/img/portfolio/task_line.svg" data-aos="zoom-in" data-aos-duration="750"></div>
 					</div>
 			
 				
