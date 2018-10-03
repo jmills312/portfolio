6aa3ac49ac6739c93feeea271f19e9c1079c2745
jared <jared@jaredmc.com>
2018-10-03 09:49:13 -0500

    Fix nav and responsiveness

--- nav.php
+++ nav.php
@@ -1,23 +1,32 @@
+<nav class="navbar-fixed-top bg navbar navbar-inverse navbar-static-top" role="navigation">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-xs-1">
+				<a href="#top"><img src="assets/img/jm-logo.svg" alt="JM logo"></a>
+			</div>
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+			</div>
 
 
-	<nav class="navbar-fixed-top bg navbar navbar-inverse navbar-static-top" role="navigation">
-		<div class="container">
-	   <div class="navbar-header">
-		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		  <span class="sr-only">Toggle navigation</span>
-		  <span class="icon-bar"></span>
-		  <span class="icon-bar"></span>
-		  <span class="icon-bar"></span>
-		</button>
-	</div>
+	  	<!-- Collect the nav links, forms, and other content for toggling -->
+	  	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="col-lg-offset-6 col-lg-5 col-md-6 col-md-offset-5 col-sm-7 col-sm-offset-4 col-xs-10 col-xs-offset-1">
+				<ul class="nav navbar-nav">
+					<li><a href="#process">Process</a></li>
+					<li><a href="#portfolio">Work</a></li>
+					<li><a href="#">Resume</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
 
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-				<li><a href="#two">Home</a></li>
-				<li><a href="#three">About</a></li>
-				<li><a href="#portfolio">Portfolio</a></li>
-				<li><a href="#contact">Contact</a></li>
-			</ul>
-		</div>
-	</nav>
+
+		</div><!-- /row-->
+	</div><!-- /container-->
+</nav>
