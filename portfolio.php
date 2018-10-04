2a4d3e6fdd5fab358ae8aa611380325b0ba0a934
jared <jared@jaredmc.com>
2018-10-04 14:12:02 -0500

    adjust new portfolio items

--- portfolio.php
+++ portfolio.php
@@ -11,19 +11,19 @@
 			</div>
 		</div>
 		<!-- portfolio items -->
-		<div class="row folio-items" data-aos="fade-up"
+		<div class="row folio-items m-bottom2" data-aos="fade-up"
      data-aos-duration="1000">
 			<a href="folio1.php">
-				<div class="col-xs-12 folio">
+				<div class="col-xs-12 folio folio1">
 					<img src="assets/img/portfolio/onboarding-imac.png" alt="onboarding dasboard" >
 				</div><!--/col xs 12-->
 			</a>
 		</div><!--/row-->
 
 		<!-- portfolio items -->
-		<div class="row " data-aos="fade-left" data-aos-duration="1000">
-			<div class="col-sm-12">
-				<img  class="img-responsive" src="assets/img/portfolio/sl-case-study.png" alt="onboarding dasboard" >
+		<div class="row m-bottom2" data-aos="fade-left" data-aos-duration="1000">
+			<div class="col-sm-6 springleaf-mocks folio">
+				<img  class="img-responsive" src="assets/img/portfolio/sl-case-study.png" alt="springleaf concepts" >
 			</div>
 
 		</div><!--/row-->
