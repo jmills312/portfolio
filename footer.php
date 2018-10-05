8d1a0cf3592939bd5a93a27c45b323a937fb9344
jared <jared@jaredmc.com>
2018-10-04 21:19:37 -0500

    Add email icon and email client

--- footer.php
+++ footer.php
@@ -11,6 +11,14 @@
 				<div class="border-bottom white-border"></div>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-xs-2">
+				<a href="mailto:jared@jaredmc.com?Subject=Hello" target="blank"><span class="glyphicon glyphicon-envelope"></span></a>
+			</div>
+			<div class="col-xs-2" class="social">
+				<a href=""><img src="assets/img/social/insta-icon.png"></a>
+			</div>
+		</div>
 		</div><!-- container-->
 	</section>
 </footer>
