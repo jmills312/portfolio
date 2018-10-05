2b35554a64d5d1bc2119f04985393d1b2dc1d2ea
jared <jared@jaredmc.com>
2018-10-05 09:29:44 -0500

    Add new assets n finish layout for index

--- footer.php
+++ footer.php
@@ -11,12 +11,24 @@
 				<div class="border-bottom white-border"></div>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-xs-2">
+
+		<div class="row m-bottom">
+			<div class="col-xs-1 col-sm-offset-4">
 				<a href="mailto:jared@jaredmc.com?Subject=Hello" target="blank"><span class="glyphicon glyphicon-envelope"></span></a>
 			</div>
-			<div class="col-xs-2" class="social">
-				<a href=""><img src="assets/img/social/insta-icon.png"></a>
+			<div class="col-xs-1">
+				<a href="https://www.linkedin.com/in/jaredmc" target="blank"><i class="fab fa-linkedin-in fa-2x"></i></a>
+			</div>
+			<div class="col-xs-1">
+				<a href="https://www.linkedin.com/in/jaredmc" target="blank"><i class="fab fa-instagram fa-2x"></i></a>
+			</div>
+			<div class="col-xs-1">
+				<a href="https://www.linkedin.com/in/jaredmc" target="blank"><i class="fab fa-twitter fa-2x"></i></a>
+			</div>
+		</div><!--/row-->
+		<div class="row m-bottom">
+			<div class="col-xs-4 col-sm-offset-4 text-center">
+				<p><span class="small">&copy; 2018 jmMills llc</span></p>
 			</div>
 		</div>
 		</div><!-- container-->
