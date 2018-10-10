cffc3b320f355711cfa5170f79b79e175ddbde76
jared <jared@jaredmc.com>
2018-10-10 10:04:26 -0500

    Add new project

--- portfolio.php
+++ portfolio.php
@@ -24,14 +24,70 @@
 		
 		<div class="row folio-items m-bottom2" data-aos="fade-up"
      data-aos-duration="1000">
-			<a href="folio2.php">	
+			<a data-target="#myModal4" data-toggle="modal">
 				<div class="col-xs-12 folio folio1">
-					<figure>
-						<img class="img-responsive onboard-dash" src="assets/img/portfolio/media-tags.png" alt="Conversant media tagging project" >
-					</figure>
+					<img class="img-responsive onboard-dash" src="assets/img/portfolio/media-tags.png" alt="Conversant media tagging project" >
 				</div><!--/col xs 12-->
 			</a>
 		</div><!--/row-->
+		<!-- SAVEUP MODAL -->
+		<div tabindex="-1" class="modal fade" id="myModal4" role="dialog" aria-hidden="true" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		      	<div class="row">
+		      		<div class="col-xs-4">
+		        	<h4 class="modal-title" id="myModalLabel">Conversant Media Tags</h4>
+		        	</div>
+		        	<div class="col-sm-2 col-sm-offset-6">
+		        		<button class="btn btn-modal close" type="button" data-dismiss="modal">X</button>
+		        	</div>
+		        </div><!--row-->
+		      </div>
+		      <div class="modal-body">
+		      	<div class="container-fluid">
+		      		<div class="row m-bottom2">
+		      			<div class="col-sm-12">
+		      				<img class="img-responsive" src="assets/img/portfolio/modal/saveup.png" alt="saveup screens">
+		      			</div>
+		      		</div>
+		      		<div class="row m-bottom2">
+		      			<div class="col-sm-2">
+		        			<h5>Role</h5>
+		        			<p>UI/UX</p>
+		        		</div>
+		      			<div class="col-sm-3">
+		        			<h5>Technologies</h5>
+		        			<p>Pencil & paper, whiteboard, Axure</p>
+		        		</div>
+								<div class="col-sm-3">
+									<h5>Timeline</h5>
+									<p>&ast;2 weeks</p>
+								</div>
+		      		</div>
+		      		<div class="row m-bottom2">
+		      			<div class="col-sm-7">
+		      				<p><span class="bold-inline">Background:</span> SaveUp was a financial app whose mission was to incentivize saving. The business model utilized a gamification approach - users enter their banking information, earn pionts through saving money (banking and 401k), and use the points to win prizes. </p>
+		      				
+		      			</div>
+		      		</div>
+		      		<div class="row m-bottom2">
+		      			<div class="col-sm-9">
+		      				<p class="m-bottom"><span class="bold-inline">Role:</span> As the lead UI/UX designer I was responsible for the redesign of the iOS, Android, and web application. Using one week sprints, I worked closely with the engineering team to roll out an mvp for iOS and Android. </p>
+		      				<p>*Engineering efforts ceased when adequate funding couldn't be obtained.</p> 
+		      			</div>
+		      		</div>
+		      	</div>
+		      </div>
+		      <div class="modal-footer">
+		        <button class="btn btn-modal" type="button" data-dismiss="modal">Close</button>
+		      </div>
+		    </div><!-- /.modal-content -->
+		  </div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->
+
+
+
 
 		<div class="row m-bottom2">
 			<a data-target="#myModal" data-toggle="modal">
@@ -103,67 +159,65 @@
 					<img  class="img-responsive" src="assets/img/portfolio/sl-case-study.png" alt="springleaf concepts" >
 				</div>
 			</a>
-
-
-		<!-- SPRINGLEAF MODAL -->
-		<div tabindex="-1" class="modal fade" id="myModal2" role="dialog" aria-hidden="true" aria-labelledby="myModalLabel">
-		  <div class="modal-dialog">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		      	<div class="row">
-		      		<div class="col-xs-4">
-		        	<h4 class="modal-title" id="myModalLabel">Springleaf Financial</h4>
-		        	</div>
-		        	<div class="col-sm-2 col-sm-offset-6">
-		        		<button class="btn btn-modal close" type="button" data-dismiss="modal">X</button>
-		        	</div>
-		        </div><!--row-->
-		      </div>
-		      <div class="modal-body">
-		      	<div class="container-fluid">
-		      		<div class="row m-bottom2">
-		      			<div class="col-sm-12">
-		      				<img class="img-responsive" src="assets/img/portfolio/modal/folioFourteenPopup.jpg" alt="saveup screens">
-		      			</div>
-		      		</div>
-		      		<div class="row m-bottom2">
-		      			<div class="col-sm-2">
-		        			<h5>Role</h5>
-		        			<p>UI/UX</p>
-		        		</div>
-		      			<div class="col-sm-2 col-sm-offset-1">
-		        			<h5>Technologies</h5>
-		        			<p>Sketch, PS, AI, HTML, CSS, JS</p>
-		        		</div>
-								<div class="col-sm-2 col-sm-offset-1">
-									<h5>Timeframe</h5>
-									<p> ~ 2 years</p>
-								</div>
-		      		</div>
-		      		<div class="row m-bottom2">
-		      			<div class="col-sm-7">
-		      				<p><span class="bold-inline">Background:</span> Springleaf Financial is a financial services company based in Evansville, Indiana. The Chicago office I worked in was a remote digital marketing team assembled in early 2016. The office expanded to roughly forty five people while I worked there. Some of the teams goals:</p>
-		      				<ul>
-		      					<li>Improve messaging to target market</li>
-		      					<li>Increase lead generation</li>
-		      					<li>Consistent branding and aesthetics</li>
-		      				</ul>
-		      			</div>
-		      		</div>
-		      		<div class="row m-bottom2">
-		      			<div class="col-sm-9">
-		      				<p class="m-bottom"><span class="bold-inline">Role:</span> My primary role was the implementation of digital marketing campaigns. Secondly, I worked on designing website pages, landing pages,  and an internal reporting app for the marketing team.</p> 
-		      			</div>
-		      		</div>
-		      	</div>
-		      </div>
-		      <div class="modal-footer">
-		        <button class="btn btn-modal" type="button" data-dismiss="modal">Close</button>
-		      </div>
-		    </div><!-- /.modal-content -->
-		  </div><!-- /.modal-dialog -->
-		</div><!-- /.modal -->
-		</div>
+			<!-- SPRINGLEAF MODAL -->
+			<div tabindex="-1" class="modal fade" id="myModal2" role="dialog" aria-hidden="true" aria-labelledby="myModalLabel">
+			  <div class="modal-dialog">
+			    <div class="modal-content">
+			      <div class="modal-header">
+			      	<div class="row">
+			      		<div class="col-xs-4">
+			        	<h4 class="modal-title" id="myModalLabel">Springleaf Financial</h4>
+			        	</div>
+			        	<div class="col-sm-2 col-sm-offset-6">
+			        		<button class="btn btn-modal close" type="button" data-dismiss="modal">X</button>
+			        	</div>
+			        </div><!--row-->
+			      </div>
+			      <div class="modal-body">
+			      	<div class="container-fluid">
+			      		<div class="row m-bottom2">
+			      			<div class="col-sm-12">
+			      				<img class="img-responsive" src="assets/img/portfolio/modal/folioFourteenPopup.jpg" alt="saveup screens">
+			      			</div>
+			      		</div>
+			      		<div class="row m-bottom2">
+			      			<div class="col-sm-2">
+			        			<h5>Role</h5>
+			        			<p>UI/UX</p>
+			        		</div>
+			      			<div class="col-sm-2 col-sm-offset-1">
+			        			<h5>Technologies</h5>
+			        			<p>Sketch, PS, AI, HTML, CSS, JS</p>
+			        		</div>
+									<div class="col-sm-2 col-sm-offset-1">
+										<h5>Timeframe</h5>
+										<p> ~ 2 years</p>
+									</div>
+			      		</div>
+			      		<div class="row m-bottom2">
+			      			<div class="col-sm-7">
+			      				<p><span class="bold-inline">Background:</span> Springleaf Financial is a financial services company based in Evansville, Indiana. The Chicago office I worked in was a remote digital marketing team assembled in early 2016. The office expanded to roughly forty five people while I worked there. Some of the teams goals:</p>
+			      				<ul>
+			      					<li>Improve messaging to target market</li>
+			      					<li>Increase lead generation</li>
+			      					<li>Consistent branding and aesthetics</li>
+			      				</ul>
+			      			</div>
+			      		</div>
+			      		<div class="row m-bottom2">
+			      			<div class="col-sm-9">
+			      				<p class="m-bottom"><span class="bold-inline">Role:</span> My primary role was the implementation of digital marketing campaigns. Secondly, I worked on designing website pages, landing pages,  and an internal reporting app for the marketing team.</p> 
+			      			</div>
+			      		</div>
+			      	</div>
+			      </div>
+			      <div class="modal-footer">
+			        <button class="btn btn-modal" type="button" data-dismiss="modal">Close</button>
+			      </div>
+			    </div><!-- /.modal-content -->
+			  </div><!-- /.modal-dialog -->
+			</div><!-- /.modal -->
+		</div><!--/row-->
 
 
 		<div class="row m-bottom2">
